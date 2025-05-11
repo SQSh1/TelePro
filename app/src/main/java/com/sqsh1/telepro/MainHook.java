0eff7748e14c0ea825b8a21c647f5c01a453cdc1
SQ Shabani <132075199+SQSh1@users.noreply.github.com>
2025-05-11 10:59:46 +0330

    Create MainHook.java

---
+++ app/src/main/java/com/sqsh1/telepro/MainHook.java
@@ -0,0 +1,31 @@
+package com.sqsh1.telepro;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("org.telegram.messenger")) {
+            return;
+        }
+        XposedBridge.log("TelePro: Hooked Telegram package");
+
+        // Example: Hook method to hide online status
+        XposedHelpers.findAndHookMethod(
+            "org.telegram.messenger.MessagesController",
+            lpparam.classLoader,
+            "setUserOnline",
+            boolean.class,
+            new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    param.setResult(null); // Prevent setting online status
+                }
+            }
+        );
+    }
+}
