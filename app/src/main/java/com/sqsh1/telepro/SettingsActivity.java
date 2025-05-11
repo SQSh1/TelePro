1ae7704016b065e64e31f8855d9dda4a76803280
SQ Shabani <132075199+SQSh1@users.noreply.github.com>
2025-05-11 11:18:16 +0330

    Create SettingsActivity.java

---
+++ app/src/main/java/com/sqsh1/telepro/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.sqsh1.telepro;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
